715d824a5b53e0104dd221975a68cf8ab88e78c7
Nadina Brodt <nadina.brodt@htwg-konstanz.de>
2023-11-27 19:10:11 +0100

    system 2

--- src/main/java/org/example/System2.java
+++ src/main/java/org/example/System2.java
@@ -2,7 +2,11 @@ package org.example;
 
 import java.security.MessageDigest;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 import static java.lang.Math.pow;
+
+import java.text.SimpleDateFormat;
 
 public class System2 {
 
@@ -13,7 +17,8 @@ public class System2 {
      * System 2
      * <p>
      * length: 10 signs
-     * allowed signs: uppercase and lowercase latin letters and digits 0 to 9 with repetitions
+     * allowed signs: uppercase and lowercase latin letters and digits 0 to 9 with
+     * repetitions
      * SHA1-hash: e39156538e3e7193fdf6356469fa7414d078f97e
      */
 
@@ -29,13 +34,72 @@ public class System2 {
 
         String s2_hash = "e39156538e3e7193fdf6356469fa7414d078f97e";
 
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance("sha1");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        if (messageDigest == null)
+            return;
+
+        char[] s2_charArray = allowedSigns.toCharArray();
+        byte[] combination = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+        for (int i = 0; i < s2_sumOfAllowedSigns; i++) {
+            combination[0] = (byte) s2_charArray[i];
+            for (int j = 0; j < s2_sumOfAllowedSigns; j++) {
+                combination[1] = (byte) s2_charArray[j];
+                for (int k = 0; k < s2_sumOfAllowedSigns; k++) {
+                    combination[2] = (byte) s2_charArray[k];
+                    for (int l = 0; l < s2_sumOfAllowedSigns; l++) {
+                        combination[3] = (byte) s2_charArray[l];
+                        for (int m = 0; m < s2_sumOfAllowedSigns; m++) {
+                            combination[4] = (byte) s2_charArray[m];
+                            for (int n = 0; n < s2_sumOfAllowedSigns; n++) {
+                                combination[5] = (byte) s2_charArray[n];
+                                for (int o = 0; o < s2_sumOfAllowedSigns; o++) {
+                                    combination[6] = (byte) s2_charArray[o];
+                                    for (int p = 0; p < s2_sumOfAllowedSigns; p++) {
+                                        combination[7] = (byte) s2_charArray[p];
+                                        for (int q = 0; q < s2_sumOfAllowedSigns; q++) {
+                                            combination[8] = (byte) s2_charArray[q];
+                                            for (int r = 0; r < s2_sumOfAllowedSigns; r++) {
+                                                combination[9] = (byte) s2_charArray[r];
+
+                                                // System.out.println(new String(combination, StandardCharsets.UTF_8));
+                                                // byte[] digest_bytes = messageDigest.digest(combination);
+
+                                                if (DigestUtils.sha1Hex(combination).equals(s2_hash)) {
+                                                    String found_timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss")
+                                                            .format(new java.util.Date());
+                                                    String password = new String(combination);
+                                                    System.out.println(found_timeStamp + " Found " + password);
+                                                    return;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+
+                            }
+
+                        }
+                    }
+
+                }
+
+            }
+        }
+
         MessageDigest s2_messageDigest = null;
         try {
             s2_messageDigest = MessageDigest.getInstance("sha1");
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
-        if (s2_messageDigest == null) return;
+        if (s2_messageDigest == null)
+            return;
 
         char[] s2_charArray = allowedSigns.toCharArray();
 
@@ -49,7 +113,10 @@ public class System2 {
                                     for (int p = 0; p < s2_charArray.length; p++) {
                                         for (int q = 0; q < s2_charArray.length; q++) {
                                             for (int r = 0; r < s2_charArray.length; r++) {
-                                                String s2_combination = "" + s2_charArray[i] + s2_charArray[j] + s2_charArray[k] + s2_charArray[l] + s2_charArray[m] + s2_charArray[n] + s2_charArray[o] + s2_charArray[p] + s2_charArray[q] + s2_charArray[r];
+                                                String s2_combination = "" + s2_charArray[i] + s2_charArray[j]
+                                                        + s2_charArray[k] + s2_charArray[l] + s2_charArray[m]
+                                                        + s2_charArray[n] + s2_charArray[o] + s2_charArray[p]
+                                                        + s2_charArray[q] + s2_charArray[r];
 
                                                 s2_messageDigest.update(s2_combination.getBytes());
                                                 byte[] digest = s2_messageDigest.digest();
